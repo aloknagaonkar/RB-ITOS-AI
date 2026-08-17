cc1d7d2e268589636e559766e7bc16d444501182
aloknagaonkar <alok_in2012@live.com>
2026-08-17 17:03:55 +0530

    feat: add independent strategy bundle foundation

---
+++ red-bar-lab-standalone-rb-2.0.0-institutional-intelligence/red_bar_lab/execution/bundles/__init__.py
@@ -0,0 +1,19 @@
+from red_bar_lab.execution.bundles.bundle_model import (
+    DIRECTIONAL_REGIME,
+    RED_BAR,
+    RSI_EXTREME_REVERSAL,
+    StrategySignalBundle,
+    infer_strategy_id,
+)
+from red_bar_lab.execution.bundles.red_bar_bundle_builder import build_red_bar_bundle
+from red_bar_lab.execution.bundles.rsi_reversal_bundle_builder import build_rsi_reversal_bundle
+
+__all__ = [
+    "DIRECTIONAL_REGIME",
+    "RED_BAR",
+    "RSI_EXTREME_REVERSAL",
+    "StrategySignalBundle",
+    "infer_strategy_id",
+    "build_red_bar_bundle",
+    "build_rsi_reversal_bundle",
+]
