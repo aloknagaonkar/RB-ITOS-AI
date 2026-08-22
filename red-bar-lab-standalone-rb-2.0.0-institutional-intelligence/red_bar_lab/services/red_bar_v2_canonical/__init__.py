9b3ff0905bcd0709ead676332dbb53c405aa6362
aloknagaonkar <alok_in2012@live.com>
2026-08-23 01:54:58 +0530

    feat: export canonical persistence api

--- red-bar-lab-standalone-rb-2.0.0-institutional-intelligence/red_bar_lab/services/red_bar_v2_canonical/__init__.py
+++ red-bar-lab-standalone-rb-2.0.0-institutional-intelligence/red_bar_lab/services/red_bar_v2_canonical/__init__.py
@@ -18,20 +18,58 @@ from .models import (
     RedBarV2ParityResult,
 )
 from .parity import compare_legacy_to_canonical
+from .persistence_identity import (
+    build_canonical_bundle_event_id,
+    build_canonical_resolution_id,
+    canonical_json,
+    payload_sha256,
+)
+from .persistence_models import (
+    CanonicalBundleEventType,
+    CanonicalBundleLifecycleEvent,
+    CanonicalPersistenceConflictError,
+    CanonicalPersistenceCorruptionError,
+    CanonicalPersistenceError,
+    CanonicalPersistenceResult,
+    CanonicalPersistenceUnavailableError,
+    PersistenceOutcome,
+    PersistedRedBarV2Resolution,
+)
+from .persistence_service import RedBarV2CanonicalPersistenceService
+from .persistence_telemetry import PersistenceBenchmark, benchmark_persistence_call
+from .repository_protocol import RedBarV2CanonicalRepository
 from .resolver import benchmark_resolver_mapping, resolve_red_bar_v2_canonical
+from .sqlite_repository import SQLiteRedBarV2CanonicalRepository
 
 __all__ = [
+    "CanonicalBundleEventType",
+    "CanonicalBundleLifecycleEvent",
+    "CanonicalPersistenceConflictError",
+    "CanonicalPersistenceCorruptionError",
+    "CanonicalPersistenceError",
+    "CanonicalPersistenceResult",
+    "CanonicalPersistenceUnavailableError",
     "CanonicalResolutionError",
     "LegacyMappingError",
     "LegacyV2DecisionEvidence",
     "LegacyV2MarketMetadata",
+    "PersistenceBenchmark",
+    "PersistenceOutcome",
+    "PersistedRedBarV2Resolution",
     "RedBarV2CanonicalError",
+    "RedBarV2CanonicalPersistenceService",
+    "RedBarV2CanonicalRepository",
     "RedBarV2CanonicalResolution",
     "RedBarV2ParityResult",
+    "SQLiteRedBarV2CanonicalRepository",
+    "benchmark_persistence_call",
     "benchmark_resolver_mapping",
+    "build_canonical_bundle_event_id",
     "build_canonical_decision",
     "build_canonical_input_readiness",
+    "build_canonical_resolution_id",
     "build_legacy_v2_decision_evidence",
+    "canonical_json",
     "compare_legacy_to_canonical",
     "create_red_bar_v2_signal_bundle",
     "event_conditions",
@@ -39,5 +77,6 @@ __all__ = [
     "event_value",
     "evidence_from_event_details",
     "evidence_to_event_details",
+    "payload_sha256",
     "resolve_red_bar_v2_canonical",
 ]
